97fdc24659ea5c0c246972b11ae599fc9a373e2c
Daniel Winks <daniel.winks@gmail.com>
2021-03-05 20:25:06 -0500

    Add rootfs

---
+++ rootfs/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+find /rattler/conf/. -type f -name '*.conf' -exec cat {} + >> /rtl_433.conf
+
+echo -e "\n\noutput mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USER},pass=${MQTT_PASS},retain=${MQTT_RETAIN},devices=rtl_433[/model][/channel][/id]" >> /rtl_433.conf
+
+rtl_433 -c /rtl_433.conf
