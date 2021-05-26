ac15e6e936b067c2e3f2b4b0997d9beef03e37fd
Daniel Winks <daniel.winks@gmail.com>
2021-05-26 16:15:26 -0400

    Append to conf instead

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -4,4 +4,8 @@ find /rtl_433/conf/. -type f -name '*.conf' -exec cat {} + >> /rtl_433.conf
 
 echo -e "\n\noutput mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USER},pass=${MQTT_PASS},retain=${MQTT_RETAIN},devices=rtl_433[/model][/channel][/id]" >> /rtl_433.conf
 
-rtl_433 -c /rtl_433.conf -d ${DEV_INDEX} -f ${FREQ}
+echo -e "\n\nfrequency ${FREQ}" >> /rtl_433.conf
+
+echo -e "\n\ndevice ${DEV_INDEX}" >> /rtl_433.conf
+
+rtl_433 -c /rtl_433.conf
